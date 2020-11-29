8fd6e2ba9917a213579477f5b5347a4fe55b9c3c
kagu244 <nessywidya@gmail.com>
2020-11-29 22:26:48 +0700

    RegisterEmail

--- app/src/main/java/com/nessy/islamapp/LoginActivity.java
+++ app/src/main/java/com/nessy/islamapp/LoginActivity.java
@@ -2,13 +2,24 @@ package com.nessy.islamapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signUp=findViewById(R.id.signUp);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/nessy/islamapp/RegisterActivity.java
+++ app/src/main/java/com/nessy/islamapp/RegisterActivity.java
@@ -1,20 +1,29 @@
 package com.nessy.islamapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
-    private TextInputEditText etEmail,etPass;
+    private TextInputEditText etEmail, etPass;
     private Button btnRegister;
     private ProgressBar progressBar;
     private FirebaseAuth mAuth;
@@ -37,20 +46,42 @@ public class RegisterActivity extends AppCompatActivity {
             String email = etEmail.getText().toString().trim();
             String pass = etPass.getText().toString().trim();
 
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 //set error
                 etEmail.setError("Invalid Email");
                 etEmail.setFocusable(true);
-            }else if (pass.length()<8){
+            } else if (pass.length() < 8) {
                 etPass.setError("Password length at least 8 characters");
                 etPass.setFocusable(true);
-            }else {
+            } else {
                 registerUser(email, pass);
             }
         });
     }
 
     private void registerUser(String email, String pass) {
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success
+                        progressBar.setVisibility(View.GONE);
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(RegisterActivity.this, "Registered...\n"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+                        finish();
+                    } else {
+                        // If sign in fails
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    // ...
+                }).addOnFailureListener(e -> {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        });
 
     }
 }
